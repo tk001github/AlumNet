5b3ba60d46e45d827274df9211996901ba383d10
psycho-valker <rameez.connect@gmail.com>
2019-06-10 22:27:31 +0530

    Login functionality added

--- app/src/main/java/com/example/alumnet/Login.java
+++ app/src/main/java/com/example/alumnet/Login.java
@@ -1,34 +1,92 @@
 package com.example.alumnet;
 
 import android.content.Intent;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
-public class Login extends AppCompatActivity {
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+
+public class Login extends AppCompatActivity implements  View.OnClickListener {
 
     EditText mTextEmail;
     EditText mTextPassword;
     Button mButtonLogin;
     TextView mTextViewRegister;
+    private FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        mTextEmail = findViewById(R.id.et_email);
-        mTextPassword = findViewById(R.id.et_password);
+        mTextEmail = findViewById(R.id.et_email_login);
+        mTextPassword = findViewById(R.id.et_password_login);
         mButtonLogin = findViewById(R.id.button_login);
         mTextViewRegister = findViewById(R.id.tv_register);
-        mTextViewRegister.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent registerIntent = new Intent(Login.this,Register.class);
-                startActivity(registerIntent);
-            }
-        });
+
+        firebaseAuth = FirebaseAuth.getInstance();
+        if(firebaseAuth.getCurrentUser() != null){
+            //start Profile Activity
+            Toast.makeText(getApplicationContext(), "Successfull login!", Toast.LENGTH_SHORT).show();
+            finish();
+            startActivity(new Intent(getApplicationContext(), dashBoard.class));
+        }
+        mButtonLogin.setOnClickListener(this);
+        mTextViewRegister.setOnClickListener(this);
+    }
+
+    private void userLogin(){
+        String email = mTextEmail.getText().toString().trim();
+        String password = mTextPassword.getText().toString().trim();
+
+        if (TextUtils.isEmpty(email)) {
+            Toast.makeText(getApplicationContext(), "Enter email address!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (TextUtils.isEmpty(password)) {
+            Toast.makeText(getApplicationContext(), "Enter password!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        if (password.length() < 6) {
+            Toast.makeText(getApplicationContext(), "Password too short, enter minimum 6 characters!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        firebaseAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if(task.isSuccessful()){
+                            //start profile activity
+                            finish();
+                            Toast.makeText(getApplicationContext(), "Succesfull login!", Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(getApplicationContext(), dashBoard.class));
+                        }
+                    }
+                });
+
+    }
+
+    @Override
+    public void onClick(View view){
+        if(view == mButtonLogin){
+            userLogin();
+        }
+
+        if(view == mTextViewRegister){
+            finish();
+            startActivity(new Intent(this, Register.class));
+        }
     }
 }
